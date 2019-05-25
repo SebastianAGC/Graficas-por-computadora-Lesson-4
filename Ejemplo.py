7dad3231a9bf038e52ec263eb9a153bb30d57a7d
Alvaro Sebastián Galindo Calderón <sebastian28.gc@gmail.com>
2019-05-24 20:53:14 -0600

    Added penguin model

--- Ejemplo.py
+++ Ejemplo.py
@@ -7,7 +7,7 @@ glViewPort(0, 0, 1000, 1000)
 glClearColor(1, 1, 1)
 glClear()
 glColor(1, 1, 1)
-glLoad("model.obj", 500, 500, 500, 0)
+glLoad("PenguinBaseMesh.obj", 700, 500, 100, 0)
 glDraw()
 glFinish()
 
